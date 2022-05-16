619d080363c3e9ef839b05ab4bc4f18dadf64a8e
nicogarro <garro-nicolas@hotmail.com>
2022-05-16 17:53:52 -0300

    nombrepuesto

--- src/app/componentes/perfil/perfil.component.ts
+++ src/app/componentes/perfil/perfil.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { persona } from 'src/app/model/persona.model';
+import { PersonaService } from 'src/app/service/persona.service';
 
 @Component({
   selector: 'app-perfil',
@@ -6,13 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./perfil.component.css']
 })
 export class PerfilComponent implements OnInit {
-  name = 'Nicolas'
-  surname = 'Garro'
-  titulo = "Full Stack Developer Jr"
+  
+  persona: persona = new persona("","","","");
 
-  constructor() { }
+  constructor(public personaService : PersonaService) { }
 
   ngOnInit(): void {
+    this.personaService.getPersona().subscribe(data =>{this.persona = data})
   }
 
 }
--- src/app/service/persona.service.ts
+++ src/app/service/persona.service.ts
@@ -8,12 +8,12 @@ import { persona } from '../model/persona.model';
   providedIn: 'root'
 })
 export class PersonaService {
-    URL = 'thhp://localhost:8080/personas/';
+    URL = 'http://localhost:8080/personas';
 
     constructor(private http: HttpClient) { }
 
 
     public getPersona(): Observable<persona>{
-      return this.http.get<persona>(this.URL+'/crear');
+      return this.http.get<persona>(this.URL+'/traer/perfil');
     }
 }
